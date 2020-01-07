fdc06b80cbf2820f188b25c4b33cb3cdbcb16732
Kshitij Mishra <kshtjmishra23@gmail.com>
2020-01-07 23:53:48 +0530

    Application starting point

---
+++ src/main/java/com/academy/dashboard/Dashboard.java
@@ -0,0 +1,16 @@
+package com.academy.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dashboard {
+
+	
+	public static void main(String[] args) {
+			
+		SpringApplication.run(Dashboard.class, args);
+
+	}
+
+}
