b5a7526b8218a766f3e61efcdcc877e3aad8b5f4
Kshitij Mishra <kshtjmishra23@gmail.com>
2020-01-07 23:58:40 +0530

    Topic controller

---
+++ src/main/java/com/academy/dashboard/topics/TopicController.java
@@ -0,0 +1,38 @@
+package com.academy.dashboard.topics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	
+	@Autowired
+	private TopicService topicService;
+	
+	@RequestMapping("/topics")
+	public List<Topic> getTopics(){
+		
+		return topicService.getTopics();
+		
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+	
+	
+
+}
