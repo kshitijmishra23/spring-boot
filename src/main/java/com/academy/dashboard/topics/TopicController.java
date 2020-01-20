72805cb4430801489a8f0b303406b4e75434049b
Kshitij Mishra <kshtjmishra23@gmail.com>
2020-01-20 23:53:55 +0530

    adding views

--- src/main/java/com/academy/dashboard/topics/TopicController.java
+++ src/main/java/com/academy/dashboard/topics/TopicController.java
@@ -5,33 +5,45 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class TopicController {
 	
 	@Autowired
 	private TopicService topicService;
 	
 	@RequestMapping("/topics")
-	public List<Topic> getTopics(){
+	public String getTopics(Model model){
 		
-		return topicService.getTopics();
+		model.addAttribute("topics",topicService.getTopics());
+		return "topics";
+	}
+	
+	@RequestMapping("/addTopic")
+	public String addTopic(Model model){
 		
+		return "addTopic";
 	}
 	
 	@RequestMapping("/topics/{id}")
-	public Optional<Topic> getTopic(@PathVariable String id) {
-		return topicService.getTopic(id);
+	public String getTopic(@PathVariable String id, Model model) {
+		topicService.getTopic(id).ifPresent(topic ->  model.addAttribute("topic",topic));
+		return "topic";
 	}
 	
 	@RequestMapping(method=RequestMethod.POST, value="/topics")
-	public void addTopic(@RequestBody Topic topic) {
+	public String addTopic( Topic topic, Model model) {
 		topicService.addTopic(topic);
+		model.addAttribute("topics",topicService.getTopics());
+		return "topics";
+		
 	}
 	
 	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
@@ -44,6 +56,7 @@ public class TopicController {
 	public void deleteTopic(@PathVariable String id) {
 		 topicService.deleteTopic(id);
 	}
+	
 	
 
 }
--- src/main/java/com/academy/dashboard/welcome/WelcomeController.java
+++ src/main/java/com/academy/dashboard/welcome/WelcomeController.java
@@ -1,14 +1,17 @@
 package com.academy.dashboard.welcome;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class WelcomeController {
 
 	@RequestMapping("/")
-	public String dashboard() {
-		return "Welcome to the Scaler Academy!";
+	public String dashboard(Model model) {
+		model.addAttribute("welcome","Hello! Welcome to the Scaler Academy!");
+		return "welcome";
 	}
 	
 }
