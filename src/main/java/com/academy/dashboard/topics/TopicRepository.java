9bbac0d643093c70c8f5947648fc984558b3f02f
Kshitij Mishra <kshtjmishra23@gmail.com>
2020-01-20 23:56:12 +0530

    nit

--- src/main/java/com/academy/dashboard/topics/TopicRepository.java
+++ src/main/java/com/academy/dashboard/topics/TopicRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface TopicRepository extends CrudRepository<Topic, String>{
 
+	public Topic findByTitle(String title);
 	
 	//getTopics
 	//getTopic(id)
