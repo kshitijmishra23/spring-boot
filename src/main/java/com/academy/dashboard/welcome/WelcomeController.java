c248122ad69dff8d708dfeb6b107818717ae069e
Kshitij Mishra <kshtjmishra23@gmail.com>
2020-01-22 00:45:32 +0530

    added logging

--- src/main/java/com/academy/dashboard/welcome/WelcomeController.java
+++ src/main/java/com/academy/dashboard/welcome/WelcomeController.java
@@ -1,15 +1,21 @@
 package com.academy.dashboard.welcome;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.qos.logback.classic.Logger;
+
 @Controller
 public class WelcomeController {
 
+	
+	Logger logger = (Logger) LoggerFactory.getLogger(WelcomeController.class);
 	@RequestMapping("/")
 	public String dashboard(Model model) {
+		logger.info("Error logged here!!");
 		model.addAttribute("welcome","Hello! Welcome to the Scaler Academy!");
 		return "welcome";
 	}
